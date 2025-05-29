d4180062c4fdbcf9a157ec239543e626399a68f1
Joestarya <aryarudysyahputra@gmail.com>
2025-05-29 18:23:00 +0700

    ini ada yang baru

--- GroomingKcg/src/control/controller.java
+++ GroomingKcg/src/control/controller.java
@@ -1,45 +1,81 @@
 package control;
-
-import java.sql.*;
-import java.util.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.time.LocalDateTime;
-import connect.connector;
-import model.*;
-import DAOImplement.implementasi;
+import java.util.List;
 import DAOgroom.catGroomDAO;
-import view.*;
+import DAOImplement.implementasi;
+import model.*;
+import view.MainView;
+import javax.swing.table.DefaultTableModel;
+import java.sql.*;
+import javax.swing.JOptionPane;
+import connect.connector;
 
-/**
- *
- * @author Snndita
- */
 public class controller {
     MainView frame;
     implementasi impDAO;
     List<modelData> md;
+
     public controller(MainView frame) {
         this.frame = frame;
         impDAO = new catGroomDAO();
         md = impDAO.getAll();
     }
-    
+
     public void isitabel(){
         md = impDAO.getAll();
         GroomData m = new GroomData(md);
-        //frame.framekamuuini().setModel(m);
+        //frame.getTabelData().setModel(m);
     }
-    
+
     public void insert(){
-        
+        modelData data = new modelData();
+        data.setNama_pelanggan(frame.getNama_pelanggan().getText());
+        data.setPaket(frame.getPaket().getText());
+        data.setDurasi(frame.getDurasi().getText());
+        data.setJadwal(frame.getJadwal().getText());
+        impDAO.insert(data);
     }
-    
-    public void edit(){
-        
+
+    public void update(){
+        modelData data = new modelData();
+        data.setNama_pelanggan(frame.getNama_pelanggan().getText());
+        data.setPaket(frame.getPaket().getText());
+        data.setDurasi(frame.getDurasi().getText());
+        data.setJadwal(frame.getJadwal().getText());
+        data.setId_layanan(Integer.parseInt(frame.getId_layanan().getText()));
+        impDAO.update(data);
     }
-    
-    public void edelete(){
-        
+
+    public void delete(){
+        int id_layanan = Integer.parseInt(frame.getId_layanan().getText());
+        impDAO.delete(id_layanan);
+    }
+
+    public void search(String column, String keyword){
+        DefaultTableModel model = new DefaultTableModel();
+        model.addColumn("id_layanan");
+        model.addColumn("nama_pelanggan");
+        model.addColumn("paket");
+        model.addColumn("durasi");
+        model.addColumn("jadwal");
+
+        try {
+            Connection conn = connector.connection();
+            String sql = "SELECT * FROM grooming WHERE " + column + " LIKE ?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, "%" + keyword + "%");
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()){
+                model.addRow(new Object[]{
+                    rs.getInt("id_layanan"),
+                    rs.getString("nama_pelanggan"),
+                    rs.getString("paket"),
+                    rs.getString("durasi"),
+                    rs.getString("jadwal"),
+                });
+            }
+            //frame.getTabelData().setModel(model);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error : " + e.getMessage());
+        }
     }
 }
--- GroomingKcg/src/model/GroomData.java
+++ GroomingKcg/src/model/GroomData.java
@@ -24,20 +24,14 @@ public class GroomData extends AbstractTableModel {
 
     @Override
     public String getColumnName(int column) {
-        switch(column){
-            case 0:
-                return "id_layanan";
-            case 1:
-                return "nama_pelanggan";
-            case 2:
-                return "paket";
-            case 3:
-                return "durasi";
-            case 4:
-                return "jadwal";
-            default:
-                return null;
-        }
+        return switch (column) {
+            case 0 -> "id_layanan";
+            case 1 -> "nama_pelanggan";
+            case 2 -> "paket";
+            case 3 -> "durasi";
+            case 4 -> "jadwal";
+            default -> null;
+        };
     }
 
     @Override
